cb25a47efd20bb814e94a5af6e2ab5ae473feea5
starvoiddev <50586934+starvoidd@users.noreply.github.com>
2023-10-17 21:40:40 +0200

    Update Terminal.tsx

--- src/apps/Terminal/Terminal.tsx
+++ src/apps/Terminal/Terminal.tsx
@@ -16,10 +16,8 @@ export default function Terminal() {
       <div className="bg-zinc-800 w-full h-full md:h-96 rounded-b-md">
           <div className='flex flex-col justify-end'>
             <div className='font-mono w-full h-full md:h-96 p-3 text-white overflow-y-scroll noscrollbar' style={{height: window.innerWidth < 768 ? window.innerHeight-100 : 385}} id={termId}>
-              <p className='text-gray-400 mb-5'><span class='gradient-text'>Welcome to PepsiOS 1.0.0 (x64)</span><br/><br/>
-                * Website:  https://xshadow.xyz<br/>
-                * Support:  hello@xshadow.xyz</p>
-              <p className='text-gray-500 text-xs'>(i) Type "help" to start! To type commands, click on "help" below. It's the input box!</p>
+              <p className='text-gray-400 mb-5'><span class='gradient-text'>Üdv a BorjuOS-ben! (x64)</span><br/><br/>
+              <p className='text-gray-500 text-xs'>(i) Segítséghez írd be a "help" parancsot.</p>
               {history.map((value) => {
                 const Component = value;
                 // @ts-ignore
@@ -28,13 +26,13 @@ export default function Terminal() {
                 }}/>
               })}
               <p>
-                visitor@xshadow:~$ 
+                guest@undernetwork.hu:~$ 
                 <input className='ml-2 bg-zinc-800 focus:outline-none' style={{width: `${inputData.length === 0 ? 4 : inputData.length}ch`}} placeholder='help' value={inputData} onChange={(value) => {inputHandler(value.target.value)}} onKeyDown={(key) => {
                     setInputData(inputData.toLowerCase().trim());
                     if (key.keyCode === 13) { // had to use deprecated keyCode because it doesn't work on some mobile devices
                       if (inputData.length === 0) return;
                       const command = commands.filter((value) => value.name === inputData)[0];
-                      const comp = () => {return <p>visitor@xshadow:~$ {inputData}</p>}
+                      const comp = () => {return <p>guest@undernetwork.hu:~$ {inputData}</p>}
                       if (!command) {
                         setHistory([...history, comp, () => {return <p className='text-red-500'>{inputData}: command not found</p>}]);
                         setInputData('');
